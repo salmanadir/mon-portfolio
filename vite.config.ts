e75f465bc99e25d91d98ccc70a1f0dc563e3fd98
NADIR Salma <135320754+salmanadir@users.noreply.github.com>
2025-09-16 16:55:36 +0100

    Adjust base path based on production mode

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/mon-portfolio/',
+  base: mode === 'production' ? '/' : '/mon-portfolio/',
   server: {
     host: "::",
     port: 8080,
